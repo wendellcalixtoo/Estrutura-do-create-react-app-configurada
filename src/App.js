fd4bebda3fbaa29c591179d1a7627b752582de56
wendellcalixtoo <wendellcalixtoo@gmail.com>
2019-08-01 07:50:05 -0300

    chore: add de lista

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import Tabela from './Components/Tabela'
+
 function App() {
+    const autores = [{
+        nome:'Paulo',
+        livro:'React',
+        preco: '1000'
+    },
+    {
+        nome:'Daniel',
+        livro:'Java',
+        preco: '99'
+    },
+    {
+        nome:'Marcos',
+        livro:'Design',
+        preco: '150'
+    },
+    {
+        nome:'Bruno',
+        livro:'DevOps',
+        preco: '100'
+    },
+    {
+        nome:'Wendell',
+        livro:'React',
+        preco: '1000'
+    }]
     return (
         <div className="App">
-            <h1>Hello world</h1>
+            <Tabela autores = { autores }/>
         </div>
     );
 }
---
+++ src/Components/Tabela/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+const TableHead = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Autores</th>
+                <th>Livros</th>
+                <th>Preços</th>
+                <th>Remover</th>
+            </tr>
+        </thead>
+    );
+};
+
+const TableBody = props => {
+    const linhas = props.autores.map((linha, index) => {
+        return (
+            <tr>
+                <td>{linha.nome}</td>
+                <td>{linha.livro}</td>
+                <td>{linha.preco}</td>
+                <td>
+                    <button>Remover</button>
+                </td>
+            </tr>
+        );
+    });
+
+    return <tbody>{linhas}</tbody>;
+};
+
+class Tabela extends Component {
+    render() {
+        const { autores } = this.props;
+        console.log(autores);
+
+        return (
+            <table>
+                <TableHead />
+                <TableBody autores={autores} />
+            </table>
+        );
+    }
+}
+
+export default Tabela;
